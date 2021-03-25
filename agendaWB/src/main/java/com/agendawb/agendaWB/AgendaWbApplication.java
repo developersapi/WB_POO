6c3d1ba2ba7fa2386440c970d9d71f211425eefa
gpn <gpn@DESKTOP-73J6ED3.mshome.net>
2021-03-25 08:22:25 -0300

    inserindo comentarios

--- agendaWB/src/main/java/com/agendawb/agendaWB/AgendaWbApplication.java
+++ agendaWB/src/main/java/com/agendawb/agendaWB/AgendaWbApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AgendaWbApplication {
 
 	public static void main(String[] args) {
+		// outro comentario...
 		SpringApplication.run(AgendaWbApplication.class, args);
 	}
 
